e7ad64107822618da4ac3caf97b98f05ae05d153
migruedav <migruedav@hotmail.com>
2023-03-22 10:18:53 -0600

    update 5 files and delete 1 file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,54 @@
-import React, { Component } from 'react'
-import LineChart from './graficos/GraficoLinea';
+import React, { Component } from "react";
+import MiComponente from "./MiComponente";
 
 const App = () => {
-  
+  const reyes = [
+    {
+      nombre: "Atanagildo",
+      reinado: 15,
+      vacasComidas: 9,
+    },
+    {
+      nombre: "Ervigio",
+      reinado: 7,
+      vacasComidas: 3,
+    },
+    {
+      nombre: "Ataúlfo",
+      reinado: 5,
+      vacasComidas: 16,
+    },
+    {
+      nombre: "Leovigildo",
+      reinado: 18,
+      vacasComidas: 3,
+    },
+    {
+      nombre: "Sisebuto",
+      reinado: 9,
+      vacasComidas: 13,
+    },
+    {
+      nombre: "Recesvinto",
+      reinado: 19,
+      vacasComidas: 11,
+    },
+    {
+      nombre: "Teodorico",
+      reinado: 33,
+      vacasComidas: 12,
+    },
+  ];
 
-  return (
-    <div>
-      <LineChart/>
-      <LineChart/>
-      <LineChart/>
-      <LineChart/>
-      <LineChart/>
-    </div>
-  )
-}
+  const url = "https://html6.es/img/rey_"
+  const componentes = reyes.filter((rey) => rey.nombre.startsWith('A'))
+  .map((rey) => (
+    <MiComponente key={rey.nombre} nombre={rey.nombre} vacas={(rey.vacasComidas)*(rey.reinado)*365} años={rey.reinado} imagen = {`${url}${rey.nombre}.png`}  />
+  ));
 
-export default App
+  console.log(componentes)
+
+  return <div className="cardContainer">{componentes.length==0 ? "No hay reyes que empiecen con esa letra":componentes}</div>;
+};
+
+export default App;
--- src/MiComponente.jsx
+++ src/MiComponente.jsx
@@ -1,17 +1,21 @@
 import React from "react";
-import './App.css'
+import "./App.css";
 
-function MiComponente({nombre,imagen,precio,color}) {
+function MiComponente({ nombre, vacas, años, imagen }) {
+  const eliminar = (e) => {
+    e.target.parentNode.remove();
+  };
+
   return (
-    <div className="caja" style={{backgroundColor:color}}>
-        <h1>{nombre}</h1>
-        <img src={imagen} alt="" />
-        <p>Precio:</p>
-        <p>{precio}</p>
-        <button>Comprar</button>
+    <div className="caja" style={{ backgroundColor: "goldenrod" }}>
+      <p>
+        <span>{nombre}</span> ha comido {vacas} vacas en sus {años} años de
+        reinado
+      </p>
+      <img src={imagen} alt="" />
+      <button onClick={eliminar}>REMOVER</button>
     </div>
   );
 }
-
 
 export default MiComponente;
